3b8d30b1635d3ac1c9e0e488f2a90437897be5ad
Roman Yakushev <boxboxbox@inbox.ru>
2023-03-19 13:02:42 +0300

    Добавьте файлы проекта.

---
+++ OrderApiApp/Model/Entity/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderApiApp.Model.Entity
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Client()
+        {
+            Name = "";
+        }
+    }
+}
