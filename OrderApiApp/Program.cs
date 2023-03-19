ef26dc86d0e14a513501903612e75c04dedd436f
Roman Yakushev <boxboxbox@inbox.ru>
2023-03-19 13:32:16 +0300

    Add NuGet Packages

--- OrderApiApp/Program.cs
+++ OrderApiApp/Program.cs
@@ -1,6 +1,20 @@
+using OrderApiApp.Model.Entity;
+using OrderApiApp.Service.ClientService;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+// обработчики тестирования бизнес-логики для работы с клиентом
+app.MapGet("/client/all", async (IDaoClient daoClient) =>
+{
+    return await daoClient.GetAllAsync();
+});
+
+app.MapPost("/client/add", async (Client client, IDaoClient daoClient) =>
+{
+    return await daoClient.AddAsync(client);
+});
---
+++ OrderApiApp/Service/ClientService/IDaoClient.cs
@@ -0,0 +1,13 @@
+﻿using OrderApiApp.Model.Entity;
+
+namespace OrderApiApp.Service.ClientService
+{
+    public interface IDaoClient
+    {
+        Task<List<Client>> GetAllAsync();
+        Task<Client> AddAsync(Client client);
+        Task<Client> UpdateAsync(Client client);
+        Task<Client> DeleteAsync(int id);
+        Task<Client> GetAsync(int id);
+    }
+}
